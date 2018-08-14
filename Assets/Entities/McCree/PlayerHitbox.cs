846fc53c32c17edde49a65c97d21e26fe63cd81b
Andrei Andreev <Razenpok@outlook.com>
2018-08-14 02:05:17 +0200

    Loop music, add red flashing on damage

--- Assets/Entities/McCree/PlayerHitbox.cs
+++ Assets/Entities/McCree/PlayerHitbox.cs
@@ -1,12 +1,51 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerHitbox : MonoBehaviour
 {
-    [SerializeField] private GameObject gunFather;
+    private bool wasHitRecently;
 
     public void Hit(Bullet bullet)
     {
-        FindObjectOfType<Inventory>().DropCarrot();
+        if (!wasHitRecently)
+        {
+            wasHitRecently = true;
+            FindObjectOfType<Inventory>().DropCarrot();
+            StartCoroutine(FlashRed());
+        }
+    }
+
+    private IEnumerator FlashRed()
+    {
+        SetSpritesColor(Color.red);
+        yield return new WaitForSeconds(0.2f);
+        SetSpritesColor(Color.white);
+        yield return new WaitForSeconds(0.2f);
+        SetSpritesColor(Color.red);
+        yield return new WaitForSeconds(0.2f);
+        SetSpritesColor(Color.white);
+        yield return new WaitForSeconds(0.2f);
+        SetSpritesColor(Color.red);
+        yield return new WaitForSeconds(0.2f);
+        SetSpritesColor(Color.white);
+        wasHitRecently = false;
+    }
+
+    private void SetSpritesColor(Color color)
+    {
+        foreach (var sprite in GetSprites())
+        {
+            sprite.color = color;
+        }
+    }
+
+    private IEnumerable<SpriteRenderer> GetSprites()
+    {
+        yield return GetComponent<SpriteRenderer>();
+        foreach (var spriteRenderer in GetComponentsInChildren<SpriteRenderer>())
+        {
+            yield return spriteRenderer;
+        }
     }
 }
