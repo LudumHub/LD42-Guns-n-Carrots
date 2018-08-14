3d02618aab5fc5bcfe35baf2e4a08ca75ef740cc
Andrei Andreev <Razenpok@outlook.com>
2018-08-14 02:19:33 +0200

    Add BG music to endscreen

---
+++ Assets/Entities/McCree/Endgame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Endgame : MonoBehaviour
+{
+    [SerializeField] private AudioSource bang1;
+    [SerializeField] private AudioSource bang2;
+
+    private void Start()
+    {
+        StartCoroutine(BangRandomly(bang1));
+        StartCoroutine(BangRandomly(bang2));
+    }
+
+    private IEnumerator BangRandomly(AudioSource bang)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.value * 2f + 0.5f);
+            bang.Play();
+        }
+    }
+}
